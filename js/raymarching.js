568e621210e642faab82bd4724492cda1d9a5cf7
Scrip0 <lavrenenko.2003@gmail.com>
2022-09-05 11:07:23 -0400

    Added shades and light

--- js/raymarching.js
+++ js/raymarching.js
@@ -1,6 +1,6 @@
 // Here I put my constants
-const MAX_RAYS = 1000 // Max raymarching steps
-const MAX_DIST = 1000.0 // Max distance for the rays
+const MAX_RAYS = 100 // Max raymarching steps
+const MAX_DIST = 100.0 // Max distance for the rays
 const HIT_DIST = 0.01 // Distance at which ray is considered to be collided with an onbject
 
 var canvas = document.getElementById("canvas");
@@ -8,6 +8,7 @@ var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 var ctx = canvas.getContext("2d");
 var canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+var time = 0;
 
 function drawPixel(x, y, r, g, b, a) {
     var index = (x + y * canvasWidth) * 4;
@@ -22,6 +23,29 @@ function updateCanvas() {
     ctx.putImageData(canvasData, 0, 0);
 }
 
+const Vec2 = function (x=0, y=0) {
+    if( x.type === 'vec2' ) return x  
+    const v = Object.create( Vec2.prototype )
+    if( Array.isArray( x ) ) {
+      v.x = x[0]; v.y = x[1]; 
+    } else if( y === undefined ) {
+      v.x = v.y = x
+    }else{
+      v.x = x; v.y = y; 
+    }
+  
+    return v
+  }
+  
+  Vec2.prototype = {
+    type: 'vec2',
+      emit() { return "vec2(" + this.x + "," + this.y + ")" },
+    emit_decl() { return ""; },
+    copy() {
+      return Vec2( this.x, this.y )
+    }
+  }
+
 class vec3 {
     constructor(x, y, z) {
         this.x = parseFloat(x);
@@ -29,7 +53,7 @@ class vec3 {
         this.z = parseFloat(z);
     }
     normalize() {
-        var m = Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+        var m = vec_length(this);
         if (m > 0) {
             return new vec3(this.x / m, this.y / m, this.z / m);
         }
@@ -47,6 +71,10 @@ function substract(a, b) {
     return new vec3(a.x - b.x, a.y - b.y, a.z - b.z);
 }
 
+function substractVec3FromNum(a, b) {
+    return new vec3(a - b.x, a - b.y, a - b.z);
+}
+
 class vec4 {
     constructor(x, y, z, w) {
         this.x = parseFloat(x);
@@ -60,7 +88,11 @@ class vec4 {
 }
 
 function vec_length(x) {
-    return Math.sqrt(x.x ** 2 + x.y ** 2 + x.z ** 2);
+    return Math.sqrt(x.x * x.x + x.y * x.y + x.z * x.z);
+}
+
+function dot(a, b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
 function getDist(p) {
@@ -71,6 +103,31 @@ function getDist(p) {
 
     d = Math.min(sphereDist, planeDist);
     return d;
+}
+
+function getNormal(p) {
+    var d = getDist(p);
+    var acc = 0.01;
+
+    var n = substractVec3FromNum(d, new vec3(
+        getDist(substract(p, new vec3(acc, 0, 0))),
+        getDist(substract(p, new vec3(0, acc, 0))),
+        getDist(substract(p, new vec3(0, 0, acc)))
+    ));
+
+    return n.normalize();
+}
+
+function getLight(p) {
+    var lightPos = new vec3(3, 5, 2);
+    var l = substract(lightPos, p).normalize();
+    var n = getNormal(p);
+    var dif = dot(n, l);
+    var d = rayMarch(sum(p, n.multiplyByN(0.1)), l);
+    if (d < vec_length(substract(lightPos, p))) {
+        dif *= 0.1
+    }
+    return dif;
 }
 
 function rayMarch(ro, rd) {
@@ -87,12 +144,15 @@ function rayMarch(ro, rd) {
 }
 
 function render() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
     var ro = new vec3(0, 1, 1);
     for (let x = 0; x < canvasWidth; x++) {
         for (let y = 0; y < canvasHeight; y++) {
             var rd = new vec3((x - canvasWidth / 2) / canvasWidth, -(y - canvasHeight / 2) / canvasWidth, 1).normalize();
-            var d = 256 - rayMarch(ro, rd);
-            drawPixel(x, y, d, d, d, 256.0);
+            var d = rayMarch(ro, rd)
+            var p = sum(ro, rd.multiplyByN(d));
+            var col = getLight(p) * 256;
+            drawPixel(x, y, col, col, col, 256.0);
         }
     }
     updateCanvas();
